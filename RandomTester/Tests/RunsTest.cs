2a8cd2fcc172e7ea379eb4c1af781730e2428fc3
Dillon Newell <dillon_newell@live.ca>
2019-04-04 16:27:04 -0400

    enumerable runs check

--- RandomTester/Tests/RunsTest.cs
+++ RandomTester/Tests/RunsTest.cs
@@ -14,23 +14,57 @@ namespace RandomTester.Tests
 
             var prop = numOnes / numBits;
 
-            var runs = 0ul;
-            bool? last = null;
-            foreach (var b in bytes)
-            {
-                for (var i = 0; i < 8; i++)
-                {
-                    var current = (b & 1 << i) != 0;
-                    if (current != last) runs++;
-                    last = current;
-                }
-            }
+            var runs = RunCheckEnum(bytes)
+                .AsParallel()
+                .LongCount(b => b);
+            //bool? last = null;
+            //foreach (var b in bytes)
+            //{
+            //    for (var i = 0; i < 8; i++)
+            //    {
+            //        var current = (b & 1 << i) != 0;
+            //        if (current != last) runs++;
+            //        last = current;
+            //    }
+            //}
 
             var num = Math.Abs(runs - 2 * numBits * prop * (1 - prop));
             var denom = 2 * Math.Sqrt(2d * numBits) * prop * (1 - prop);
             var pValue = Helpers.ErrorFuncCompl(num / denom);
 
             return new TestResults(nameof(RunsTest), pValue);
+        }
+
+        private static IEnumerable<bool> RunCheckEnum(IEnumerable<byte> bytes)
+        {
+            using (var e1 = bytes.GetEnumerator())
+            {
+                if (!e1.MoveNext()) throw new ArgumentException();
+
+                yield return true;
+
+                while (true)
+                {
+                    for (var i = 0; i < 8; i++)
+                    {
+                        var first = (e1.Current & 1 << i) != 0;
+
+                        bool second;
+                        if (i < 7)
+                        {
+                            second = (e1.Current & 1 << (i + 1)) != 0;
+                        }
+                        else
+                        {
+                            if (!e1.MoveNext()) yield break;
+
+                            second = (e1.Current & 1) != 0;
+                        }
+
+                        yield return first != second;
+                    }
+                }
+            }
         }
     }
 }
